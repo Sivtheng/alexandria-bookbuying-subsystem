02df385e54ae097223f1abadbc866b63818879d2
Sivtheng <sivtheng25@gmail.com>
2024-01-27 10:31:03 +0700

    hero section

---
+++ resources/views/layout/hero.blade.php
@@ -0,0 +1,15 @@
+<head>
+    <link href="{{ asset('css/hero.css') }}" rel="stylesheet">
+</head>
+<div class="hero-section">
+    <div class="text-section">
+        <h1>Welcome To</h1>
+        <h1>Alexandria</h1>
+        <h1>Book Marketplace</h1>
+    </div>
+    <div class="image-section">
+        <img src="{{ asset('images/homebook.png') }}" alt="Homebook">
+        <img src="{{ asset('images/cat.png') }}" alt="Cat" class="cat-image">
+    </div>
+</div>
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,5 @@
 @section('title', 'Alexandria')
 
 @section('content')
-    <div class="container">
-        <h1>Welcome to My Website</h1>
-        <p>This is the home page content.</p>
-    </div>
+    @include('layout.hero')
 @endsection
