8f121877122259c71b00072a34541501a6daaa1b
Sivtheng <sivtheng25@gmail.com>
2024-01-30 09:19:35 +0700

    removing unnessary use of library

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,11 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Pagination\LengthAwarePaginator;
-use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Collection;
 
 
 class BookController extends Controller
